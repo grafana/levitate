0f8371012dfe3bb238922c131edc5e1b973659ec
Marcus Andersson <marcus.andersson@grafana.com>
2021-12-17 10:39:57 +0100

    will compare with gcom.

--- .github/workflows/scripts/metrics-to-bq.js
+++ .github/workflows/scripts/metrics-to-bq.js
@@ -1,5 +1,6 @@
 module.exports = async ({ context, core }) => {
     const { BigQuery } = require('@google-cloud/bigquery');
+    const axios = require('axios');
     
     const credentials = process.env.BQ_SA_KEY;
     const projectId = process.env.BQ_PROJECT;
@@ -10,12 +11,42 @@ module.exports = async ({ context, core }) => {
       credentials: JSON.parse(credentials), 
       projectId 
     });
-    
+
     const [rows] = await client.dataset(dataset).table(table).query(`
       SELECT plugin_id, plugin_version
       FROM \`${projectId}.${dataset}.${table}\`
       GROUP BY plugin_id, plugin_version
     `);
 
-    console.log('rows', rows);
+    const pluginsToSkip = rows.reduce((toSkip, row) => {
+      toSkip[createKey(row.plugin_id, row.plugin_version)];
+      return toSkip;
+    }, {});
+
+    const { data } = await axios({
+      method: 'GET',
+      url: 'https://grafana.com/api/plugins'
+    });
+
+    const pluginsToLevitate = data.items.reduce((toLevitate, item) => {
+      const { slug, version, url } = item;
+
+      if (!toSkip[createKey(slug, version)] && existsOnGithub(url)) {
+        toLevitate.push({ slug, version, url });
+      }
+
+      return toLevitate;
+    }, []);
+
+    const output = JSON.stringify(pluginsToLevitate);
+    core.setOutput('plugins_to_levitate', output);
 };
+
+function createKey(slug, version) {
+  return `${slug}|${version}`;
+}
+
+function existsOnGithub(url) {
+  return typeof url === 'string' &&
+    url.startsWith('https://github.com');
+}
