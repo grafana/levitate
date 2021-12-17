b3fb001291e73a7ab1e2281f13a4f9f9bee7aa30
Marcus Andersson <marcus.andersson@grafana.com>
2021-12-17 10:24:58 +0100

    wip

--- .github/workflows/scripts/metrics-to-bq.js
+++ .github/workflows/scripts/metrics-to-bq.js
@@ -1,11 +1,16 @@
-module.exports = async ({ context, core, options }) => {
-    console.log('options', options);
+module.exports = async ({ context, core }) => {
     const { BigQuery } = require('@google-cloud/bigquery');
-    const { credentials, projectId } = options;
-    const table = 'grafana_imports';
-    const dataset = 'plugins_data';
+    
+    const credentials = process.env.BQ_SA_KEY;
+    const projectId = process.env.BQ_PROJECT;
+    const table = process.env.BQ_TABLE;
+    const dataset = process.env.BQ_DATASET;
 
-    const client = new BigQuery({ credentials, projectId });
+    const client = new BigQuery({ 
+      credentials: JSON.parse(credentials), 
+      projectId 
+    });
+    
     const [rows] = await client.dataset(dataset).table(table).query(`
       SELECT plugin_id, plugin_version
       FROM \`${projectId}.${dataset}.${table}\`
