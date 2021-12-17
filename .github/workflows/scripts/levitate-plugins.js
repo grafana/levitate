8e57aa5a362126ec8090d44e0da836aebff8bcd5
Marcus Andersson <marcus.andersson@grafana.com>
2021-12-17 15:15:24 +0100

    trying to create file old way.

--- .github/workflows/scripts/levitate-plugins.js
+++ .github/workflows/scripts/levitate-plugins.js
@@ -1,32 +1,41 @@
-const grafanaPackages = [
-    '@grafana/ui', 
-    '@grafana/data', 
-    '@grafana/runtime'
-];
+const grafanaPackages = ["@grafana/ui", "@grafana/data", "@grafana/runtime"];
 
 module.exports = async ({ core, exec }) => {
-    const plugins = JSON.parse(process.env.PLUGINS);
-    const table = process.env.BQ_TABLE;
-    const dataset = process.env.BQ_DATASET;
+  const fs = require("fs");
+  
+  const pluginsPath = process.env.PLUGINS_FILE_PATH;
+  const table = process.env.BQ_TABLE;
+  const dataset = process.env.BQ_DATASET;
 
-    for (const plugin of plugins) {
-        try {
-            core.startGroup(`Plugin: ${plugin.slug} (v${plugin.version})`);
-            core.info(`running levitate commands`);
+  const plugins = await readFile(fs, pluginsPath);
 
-            await exec.exec(`
+  for (const plugin of plugins) {
+    try {
+      core.startGroup(`Plugin: ${plugin.slug} (v${plugin.version})`);
+      core.info(`running levitate commands`);
+
+      await exec.exec(`
                 ./node_modules/.bin/levitate gobble \
                     --repositories ${plugin.url} \
-                    --filters ${grafanaPackages.join(' ')} \
+                    --filters ${grafanaPackages.join(" ")} \
                     --jsonlines \
                     | ./node_modules/.bin/levitate-bq \
                         --dataset ${dataset} \
                         --table ${table}
             `);
-        } catch (error) {
-            core.error(`levitate commands failed: ${error}`);
-        } finally {
-            core.endGroup();
-        }
+    } catch (error) {
+      core.error(`levitate commands failed: ${error}`);
+    } finally {
+      core.endGroup();
     }
+  }
 };
+
+function readFile(fs, path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) return reject(err);
+      return resolve(JSON.parse(data));
+    });
+  });
+}
--- .github/workflows/scripts/plugins-to-levitate.js
+++ .github/workflows/scripts/plugins-to-levitate.js
@@ -1,50 +1,64 @@
 module.exports = async () => {
-    const { BigQuery } = require('@google-cloud/bigquery');
-    const axios = require('axios');
-    
-    const credentials = process.env.BQ_SA_KEY;
-    const projectId = process.env.BQ_PROJECT;
-    const table = process.env.BQ_TABLE;
-    const dataset = process.env.BQ_DATASET;
+  const { BigQuery } = require("@google-cloud/bigquery");
+  const axios = require("axios");
+  const fs = require("fs");
 
-    const client = new BigQuery({ 
-      credentials: JSON.parse(credentials), 
-      projectId 
-    });
+  const credentials = process.env.BQ_SA_KEY;
+  const projectId = process.env.BQ_PROJECT;
+  const table = process.env.BQ_TABLE;
+  const dataset = process.env.BQ_DATASET;
 
-    const [rows] = await client.dataset(dataset).table(table).query(`
+  const client = new BigQuery({
+    credentials: JSON.parse(credentials),
+    projectId,
+  });
+
+  const [rows] = await client.dataset(dataset).table(table).query(`
       SELECT plugin_id, plugin_version
       FROM \`${projectId}.${dataset}.${table}\`
       GROUP BY plugin_id, plugin_version
     `);
 
-    const pluginsToSkip = rows.reduce((toSkip, row) => {
-      toSkip[createKey(row.plugin_id, row.plugin_version)] = true;
-      return toSkip;
-    }, {});
+  const pluginsToSkip = rows.reduce((toSkip, row) => {
+    toSkip[createKey(row.plugin_id, row.plugin_version)] = true;
+    return toSkip;
+  }, {});
 
-    const { data } = await axios({
-      method: 'GET',
-      url: 'https://grafana.com/api/plugins'
-    });
+  const { data } = await axios({
+    method: "GET",
+    url: "https://grafana.com/api/plugins",
+  });
 
-    return data.items.reduce((toLevitate, item) => {
-      const { slug, version, url } = item;
-      const key = createKey(slug, version);
+  const pluginsToLevitate = data.items.reduce((toLevitate, item) => {
+    const { slug, version, url } = item;
+    const key = createKey(slug, version);
 
-      if (!pluginsToSkip[key] && existsOnGithub(url)) {
-        toLevitate.push({ slug, version, url });
-      }
+    if (!pluginsToSkip[key] && existsOnGithub(url)) {
+      toLevitate.push({ slug, version, url });
+    }
 
-      return toLevitate;
-    }, []);
+    return toLevitate;
+  }, []);
+
+  const filePath = "./plugins.json";
+
+  await writeToFile(fs, filePath, pluginsToLevitate);
+  return filePath;
 };
+
+function writeToFile(fs, path, values) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, JSON.stringify(values), (err) => {
+      if (err) return reject(err);
+      return resolve();
+    });
+  });
+}
 
 function createKey(slug, version) {
   return `${slug}|${version}`;
 }
 
 function existsOnGithub(url) {
-  return typeof url === 'string' &&
-    url.startsWith('https://github.com');
+  return typeof url === "string" && url.startsWith("https://github.com");
 }
