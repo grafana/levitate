f8f3595b8f9b12bbaea74515f4b084b3f794e6ff
Levente Balogh <balogh.levente.hu@gmail.com>
2022-01-17 16:05:34 +0100

    test: add tests for comparing types

---
+++ src/tests/utils.compare.types.test.ts
@@ -0,0 +1,58 @@
+import { testCompare } from "./test-utils";
+
+describe("Compare types", () => {
+  test("changing a type should trigger a change", () => {
+    const prev = `
+      export declare type Foo = (value: number) => string;
+
+      export type Bar = {
+        one: string;
+        two: number;
+      }
+
+      export type Baz = {
+        three: number;
+        four: boolean;
+      }
+    `;
+    const current = `
+      export declare type Foo = (value: string) => string;
+
+      export type Bar = {
+        one: string;
+        two: number;
+        three: boolean;
+      }
+
+      export type Baz = {
+        three: number;
+        four: boolean;
+      }
+    `;
+    const comparison = testCompare(prev, current);
+
+    expect(Object.keys(comparison.changes)).toEqual(["Foo", "Bar"]);
+    expect(Object.keys(comparison.additions).length).toBe(0);
+    expect(Object.keys(comparison.removals).length).toBe(0);
+  });
+
+  test("not changing anything should not trigger a change", () => {
+    const prev = `
+      export type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const current = `
+      export type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const comparison = testCompare(prev, current);
+
+    expect(Object.keys(comparison.changes).length).toBe(0);
+    expect(Object.keys(comparison.additions).length).toBe(0);
+    expect(Object.keys(comparison.removals).length).toBe(0);
+  });
+});
