86a14a1d45acd0e5af7b770fa9b0cfdcd011b8cb
Levente Balogh <balogh.levente.hu@gmail.com>
2022-01-24 10:57:06 +0100

    Chore: expose code from the package (#32)
    * feat: expose code and types from the package
    
    * chore: update CHANGELOG
    
    * chore: remove unnecessary `jsx` setting from `tsconfig.json`

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./utils.compare";
+export * from "./utils.compiler";
+export * from "./utils.compiler.imports";
+export * from "./utils.compiler.imports";
+export * from "./utils.diff";
+export * from "./utils.npm";
