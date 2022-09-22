e81d6ceb3d66adaa4796224dbe508afc810d7653
Esteban Beltran <esteban.beltran@elastic.co>
2022-09-22 10:18:16 +0200

    Update fixtures for sub-export development

--- fixtures/exports/package1/index.ts
+++ fixtures/exports/package1/index.ts
@@ -5,4 +5,48 @@ export { default as mainInstance } from './data';
 export function inlined() {
   return 'inlined';
 }
+
+export enum FooEnum {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+}
+
+export class FooClass {
+  constructor(public foo: string) {}
+
+  private privateMethod(): string {
+    return 'private';
+  }
+
+  method1(param1: string, param2: number) {
+    return param1 + param2;
+  }
+  method2() {
+    return this.foo;
+  }
+
+  static staticMethod() {
+    return 'static';
+  }
+
+  static staticMethod2() {
+    return 'static2';
+  }
+}
+
+export class SubFooClass extends FooClass {
+  constructor(public foo: string) {
+    super(foo);
+  }
+
+  method1(param1: string, param2: number) {
+    return super.method1(param1, param2);
+  }
+
+  subMethod() {
+    return 'sub';
+  }
+}
+
 export * from './lib';
