73d87ec911bc2312428a2eb532e6c547700f40ef
Esteban Beltran <esteban@academo.me>
2023-07-26 15:49:32 +0200

    Add levignore fixture

---
+++ fixtures/levignore/.levignore.js
@@ -0,0 +1,5 @@
+module.exports = {
+  removals: [/SampleEnum\.D/],
+  changes: [/SampleEnum/],
+  additions: [/Value2/],
+};
---
+++ fixtures/levignore/package2/index.d.ts
@@ -0,0 +1,12 @@
+export enum SampleEnum {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+}
+
+export const Value1 = 'value1';
+export const Value2 = 'value2';
+
+export function TestFunction(arg1: string, arg2: number): string {
+  return arg1 + arg2;
+}
