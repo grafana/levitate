409ac5ab4d1ad7e0cba996c9392a476bf13bab49
Esteban Beltran <esteban@academo.me>
2023-11-22 11:03:15 +0100

    Add additional tests for compare

--- src/commands/compare/types.test.ts
+++ src/commands/compare/types.test.ts
@@ -55,4 +55,84 @@ describe('Compare types', () => {
     expect(Object.keys(comparison.additions).length).toBe(1);
     expect(Object.keys(comparison.removals).length).toBe(0);
   });
+
+  test('Removing "Export" from a symbol should trigger a breaking change', () => {
+    const prev = `
+      export type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const current = `
+      type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const comparison = testCompare(prev, current);
+
+    expect(Object.keys(comparison.removals)).toEqual(['Bar', 'Bar.one', 'Bar.two']);
+    expect(Object.keys(comparison.additions).length).toBe(0);
+    expect(Object.keys(comparison.removals).length).toBe(3);
+  });
+
+  test('Removing "declare" from a symbol should not trigger a breaking change', () => {
+    const prev = `
+      declare type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const current = `
+      type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const comparison = testCompare(prev, current);
+
+    expect(Object.keys(comparison.removals).length).toBe(0);
+    expect(Object.keys(comparison.additions).length).toBe(0);
+    expect(Object.keys(comparison.removals).length).toBe(0);
+  });
+
+  test('Adding "declare" from a symbol should not trigger a breaking change', () => {
+    const prev = `
+      type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const current = `
+      declare type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const comparison = testCompare(prev, current);
+
+    expect(Object.keys(comparison.removals).length).toBe(0);
+    expect(Object.keys(comparison.additions).length).toBe(0);
+    expect(Object.keys(comparison.removals).length).toBe(0);
+  });
+
+  test('', () => {
+    const prev = `
+      type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const current = `
+      declare type Bar = {
+        one: string;
+        two: number;
+      }
+    `;
+    const comparison = testCompare(prev, current);
+
+    expect(Object.keys(comparison.removals).length).toBe(0);
+    expect(Object.keys(comparison.additions).length).toBe(0);
+    expect(Object.keys(comparison.removals).length).toBe(0);
+  });
 });
